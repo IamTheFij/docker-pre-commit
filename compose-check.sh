af49bee4e5d4639307be56e621adbed7a88462bf
Ian Fijolek <ian@iamthefij.com>
2020-01-27 17:17:55 -0500

    Add some tests and drone to run them

--- compose-check.sh
+++ compose-check.sh
@@ -6,15 +6,16 @@ check_file() {
     local file=$1
     docker-compose -f "$file" config -q 2>&1 \
         | sed "/variable is not set. Defaulting/d"
-    return ${PIPESTATUS[0]}
+    return "${PIPESTATUS[0]}"
 }
 
 check_files() {
-    local all_files=$@
+    local all_files=( "$@" )
     has_error=0
-    for file in $all_files ; do
+    for file in "${all_files[@]}" ; do
         if [[ -f "$file" ]]; then
             if ! check_file "$file" ; then
+                echo "ERROR: $file"
                 has_error=1
             fi
         fi
@@ -22,7 +23,7 @@ check_files() {
     return $has_error
 }
 
-if ! check_files $@ ; then
+if ! check_files "$@" ; then
     echo "To ignore, use --no-verify"
 fi
 
