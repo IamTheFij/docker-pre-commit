098ffc1472816547bbca25fb0a9f430776d59b1e
Ian Fijolek <ian@iamthefij.com>
2023-04-18 17:19:44 -0700

    Remove hadolint completely (inclusing script)

--- hadolint-deprecation.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "Hadolint hooks have been deprecated in favor of upstream"
-echo "repo: https://github.com/hadolint/hadolint"
-echo "Replace hook 'hadolint' with 'hadolint-docker'"
-echo "Replace hook 'hadolint-system' with 'hadolint'"
-
-exit 1
